ab3eeec3e378a6e7519b7880da34492c93aa019e
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-24 16:40:27 +0300

    Add Account entity

---
+++ src/main/java/by/sakujj/model/Account.java
@@ -0,0 +1,21 @@
+package by.sakujj.model;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class Account implements Entity<String>{
+    private String id;
+    private Long clientId;
+    private Long bankIdentificationNumber;
+    private BigDecimal moneyAmount;
+    private Currency currency;
+    private LocalDate dateWhenOpened;
+}
