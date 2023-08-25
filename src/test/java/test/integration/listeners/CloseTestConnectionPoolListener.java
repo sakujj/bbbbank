ae22e1271981bd8de5296e642db2ecdfa684d0cf
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-25 16:35:11 +0300

    Add and configure TestConnectionPool

---
+++ src/test/java/test/integration/listeners/CloseTestConnectionPoolListener.java
@@ -0,0 +1,15 @@
+package test.integration.listeners;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestPlan;
+import test.integration.connectionpool.TestConnectionPool;
+
+
+@Slf4j
+public class CloseTestConnectionPoolListener implements TestExecutionListener {
+    @Override
+    public void testPlanExecutionFinished(TestPlan testPlan) {
+        TestConnectionPool.getInstance().close();
+        log.debug("Connection pool has been closed and all connections have been released");
+    }
+}
