53c7895cccd8f4b8df62ab499a13d6d314b046cd
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-28 02:21:49 +0300

    Add additional BCryptHasher tests

--- src/test/java/test/unit/BCryptHasherTests.java
+++ src/test/java/test/unit/BCryptHasherTests.java
@@ -13,11 +13,30 @@ public class BCryptHasherTests {
     Hasher hasher = BCryptHasher.getInstance();
 
     @ParameterizedTest
-    @ValueSource(strings = {"pass1", "qwertyzxcv32432@@"})
+    @ValueSource(strings = {"pass1", "qwertyzxcv32432@@", "s"})
     void shouldHashAndThenVerify(String password) {
         String hashed = hasher.hash(password);
         boolean isVerified = hasher.verifyHash(password, hashed);
 
         assertThat(isVerified).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pass1", "qwertyzxcv32432@@", "s"})
+    void differentPasswordsHaveDifferentHashes(String password) {
+        String hashed = hasher.hash(password);
+        boolean isVerified = hasher.verifyHash(password + "test", hashed);
+
+        assertThat(isVerified).isFalse();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"pass1", "qwertyzxcv32432@@", "s"})
+    void samePasswordsHaveDifferentHashes(String password) {
+        String hashedFirst = hasher.hash(password);
+        String hashedSecond = hasher.hash(password);
+
+        assertThat(hashedFirst).isNotEqualTo(hashedSecond);
     }
 }
