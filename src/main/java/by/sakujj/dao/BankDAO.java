7913e6e08e47dd8ff3707492b466eedc96838e28
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-27 12:02:40 +0300

    Change return types in DAO update, delete

--- src/main/java/by/sakujj/dao/BankDAO.java
+++ src/main/java/by/sakujj/dao/BankDAO.java
@@ -1,16 +1,13 @@
 package by.sakujj.dao;
 
-import by.sakujj.exceptions.DaoException;
+import by.sakujj.exceptions.DAOException;
 import by.sakujj.model.Bank;
-import by.sakujj.util.SqlQueries;
+import by.sakujj.util.SQLQueries;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -31,31 +28,31 @@ public class BankDAO implements DAO<Bank, Long> {
             "name"
     );
 
-    private static final String FIND_BY_ID = SqlQueries.getSelectById(
+    private static final String FIND_BY_ID = SQLQueries.getSelectById(
             TABLE_NAME,
             ID_COLUMN_NAME
     );
 
-    private static final String FIND_ALL = SqlQueries.getSelectAll(TABLE_NAME);
+    private static final String FIND_ALL = SQLQueries.getSelectAll(TABLE_NAME);
 
-    private static final String UPDATE_BY_ID = SqlQueries.getUpdateById(
+    private static final String UPDATE_BY_ID = SQLQueries.getUpdateById(
             TABLE_NAME,
             ID_COLUMN_NAME,
             ATTRIBUTES_WITHOUT_ID
     );
 
-    private static final String DELETE_BY_ID = SqlQueries.getDeleteById(
+    private static final String DELETE_BY_ID = SQLQueries.getDeleteById(
             TABLE_NAME,
             ID_COLUMN_NAME
     );
 
-    private static final String INSERT_BY_ID = SqlQueries.getInsert(
+    private static final String INSERT_BY_ID = SQLQueries.getInsert(
             TABLE_NAME,
-            ATTRIBUTES_WITHOUT_ID
+            List.of("bank_id", "name")
     );
 
     @Override
-    public Optional<Bank> findById(Long id, Connection connection) throws DaoException {
+    public Optional<Bank> findById(Long id, Connection connection) throws DAOException {
         try (PreparedStatement statement = connection.prepareStatement(FIND_BY_ID)) {
 
             statement.setObject(1, id);
@@ -70,12 +67,12 @@ public class BankDAO implements DAO<Bank, Long> {
             return Optional.empty();
 
         } catch (SQLException e) {
-            throw new DaoException(e);
+            throw new DAOException(e);
         }
     }
 
     @Override
-    public List<Bank> findAll(Connection connection) throws DaoException {
+    public List<Bank> findAll(Connection connection) throws DAOException {
         try (PreparedStatement statement = connection.prepareStatement(FIND_ALL)){
 
             ResultSet resultSet = statement.executeQuery();
@@ -87,26 +84,51 @@ public class BankDAO implements DAO<Bank, Long> {
             return all;
 
         } catch (SQLException e) {
-            throw new DaoException(e);
+            throw new DAOException(e);
         }
     }
 
     @Override
-    public Long save(Bank obj, Connection connection) {
-        throw new UnsupportedOperationException();
+    public Long save(Bank obj, Connection connection) throws DAOException {
+        try (PreparedStatement statement = connection.prepareStatement(INSERT_BY_ID)){
+            statement.setObject(1, obj.getId());
+            statement.setObject(2, obj.getName());
+
+            statement.executeUpdate();
+
+            return obj.getId();
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
-    public void update(Bank obj, Connection connection) {
-        throw new UnsupportedOperationException();
+    public boolean update(Bank obj, Connection connection) throws DAOException {
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_BY_ID)) {
+            statement.setObject(1, obj.getName());
+            statement.setObject(2, obj.getId());
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
 
     @Override
-    public void deleteById(Long id, Connection connection) {
-        throw new UnsupportedOperationException();
+    public boolean deleteById(Long id, Connection connection) throws DAOException {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID)) {
+
+            statement.setObject(1, id);
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            throw new DAOException(e);
+        }
     }
 
-    private static Bank newBank(ResultSet resultSet) throws SQLException {
+    public static Bank newBank(ResultSet resultSet) throws SQLException {
         Long id = resultSet.getObject("bank_id", Long.class);
         String name = resultSet.getObject("name", String.class);
         return Bank.builder()
