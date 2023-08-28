ea2bb1d6a1622c1594435f222807bd72ab0db8d8
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-28 12:56:13 +0300

    Add Bank response and request DTO

---
+++ src/main/java/by/sakujj/dto/BankRequest.java
@@ -0,0 +1,13 @@
+package by.sakujj.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BankRequest {
+    private String name;
+}
