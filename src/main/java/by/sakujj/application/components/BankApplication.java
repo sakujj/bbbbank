6f5a11f5f2214f7111cbd24d7eff0ecacefc13b9
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-30 23:16:06 +0300

    Add bank application

---
+++ src/main/java/by/sakujj/application/components/BankApplication.java
@@ -0,0 +1,272 @@
+package by.sakujj.application.components;
+
+import by.sakujj.context.ApplicationContext;
+import by.sakujj.dto.*;
+import by.sakujj.model.Currency;
+import by.sakujj.model.MonetaryTransactionType;
+import by.sakujj.services.*;
+import lombok.RequiredArgsConstructor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class BankApplication {
+    private static final ApplicationContext context = ApplicationContext.getInstance();
+
+    private final ClientResponse authenticatedUser;
+    private final AccountService accountService = context.getByClass(AccountService.class);
+    private final BankService bankService = context.getByClass(BankService.class);
+    private final MonetaryTransactionService monetaryTransactionService
+            = context.getByClass(MonetaryTransactionService.class);
+
+    public void start() {
+        try {
+            if (authenticatedUser == null) {
+                System.out.println("(!) Quiting");
+                return;
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String output;
+            String input;
+            do {
+                System.out.println("""
+                        Type:
+                        \t'my a' to view your accounts
+                        \t'new a' to create new account
+                        \t'all a' to see all accounts
+                        \t'all b' to see all banks
+                        \t'q' to quit
+                        \t't' to transfer money to other account
+                        \t'd' to deposit money to specified account
+                        \t'w' to withdrawal money from specified account
+                        """);
+
+                input = reader.readLine();
+                output = switch (input) {
+                    case "my a" -> getMyAccounts();
+                    case "new a" -> createNewAccount(reader);
+                    case "all a" -> getAllAccounts();
+                    case "all b" -> getAllBanks();
+                    case "d" -> depositToAccount(reader);
+                    case "w" -> withdrawFromAccount(reader);
+                    case "t" -> transferToAccount(reader);
+                    case "q" -> "Goodbye %s, %s".formatted(
+                            authenticatedUser.getUsername(),
+                            authenticatedUser.getEmail());
+                    default -> "Wrong input, try again";
+                };
+                System.out.printf("%n%s%n%n", output);
+            } while (!"q".equals(input));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String transferToAccount(BufferedReader reader) throws IOException {
+        System.out.println("Your accounts: ");
+        System.out.println(getMyAccounts());
+
+        System.out.println("Specify your account ID to TRANSFER from: ");
+        String myAccountId = reader.readLine();
+        List<AccountResponse> myAccounts = accountService.findAllByClientId(authenticatedUser.getId());
+        Optional<AccountResponse> possibleMyAccount = myAccounts.stream()
+                .filter(a -> a.getId().equals(myAccountId))
+                .findAny();
+        if (possibleMyAccount.isEmpty()) {
+            return "You can TRANSFER only from your accounts";
+        }
+
+        System.out.println("All accounts: ");
+        System.out.println(getAllAccounts());
+
+        System.out.println("Specify the ID of an account to TRANSFER to: ");
+        String otherAccountId = reader.readLine();
+        if (otherAccountId.equals(myAccountId)) {
+            return "You can not TRANSFER to the same account";
+        }
+        Optional<AccountResponse> possibleAccountToTransferTo = accountService.findById(otherAccountId);
+        if (possibleAccountToTransferTo.isEmpty())
+            return "Non existing ID specified";
+
+        var myAccount = possibleMyAccount.get();
+        var accountToTransferTo = possibleAccountToTransferTo.get();
+        if (!myAccount.getCurrency().equals(accountToTransferTo.getCurrency()))
+            return "Can not transfer %s to account with %s".formatted(myAccount.getCurrency(),
+                    accountToTransferTo.getCurrency());
+
+        System.out.print("Specify money amount to TRANSFER: ");
+        String moneyAmount = reader.readLine();
+        if (new BigDecimal(moneyAmount).compareTo(possibleMyAccount.get().getMoneyAmount()) > 0) {
+            return "Can not TRANSFER specified amount, balance is too low";
+        }
+
+        System.out.println("""
+                Are you sure to TRANSFER %s %s
+                    from your account:
+                        %s
+                    to other account:
+                        %s
+                ?
+                yes/y or no/n""".formatted(moneyAmount, myAccount.getCurrency(),
+                myAccountId,
+                otherAccountId));
+        String answer = reader.readLine();
+
+        if (!answer.equals("yes") && !answer.equals("y"))
+            return "TRANSFER canceled";
+
+        MonetaryTransactionType type = MonetaryTransactionType.TRANSFER;
+        MonetaryTransactionRequest request = MonetaryTransactionRequest.builder()
+                .moneyAmount(moneyAmount)
+                .senderAccountId(myAccountId)
+                .receiverAccountId(otherAccountId)
+                .type(type.toString())
+                .build();
+        Optional<MonetaryTransactionResponse> possibleResponse
+                = monetaryTransactionService.createTransferTransaction(request);
+        return possibleResponse.map(r -> "Successful transaction performed")
+                .orElse("Error occurred, transaction cancelled");
+    }
+
+    public String depositToAccount(BufferedReader reader) throws IOException {
+        System.out.println("Your accounts: ");
+        System.out.println(getMyAccounts());
+
+        System.out.print("Specify the id of an account to DEPOSIT: ");
+        String accountId = reader.readLine();
+
+        System.out.print("Specify money amount to DEPOSIT: ");
+        String moneyAmount = reader.readLine();
+
+        Optional<AccountResponse> possibleAccountToDeposit = accountService.findById(accountId);
+        if (possibleAccountToDeposit.isEmpty()) {
+            return "Non existing account was specified";
+        }
+        AccountResponse accountToDeposit = possibleAccountToDeposit.get();
+        System.out.printf("Are you sure you want to DEPOSIT %s %s%nto %s account of %s?%nyes/y or no/n: ", moneyAmount, accountToDeposit.getCurrency(),
+                accountId,
+                accountToDeposit.getClientEmail());
+        String answer = reader.readLine();
+
+        if (!answer.equals("yes") && !answer.equals("y"))
+            return "DEPOSIT canceled";
+
+        MonetaryTransactionType type = MonetaryTransactionType.DEPOSIT;
+        MonetaryTransactionRequest request = MonetaryTransactionRequest.builder()
+                .senderAccountId(null)
+                .receiverAccountId(accountId)
+                .type(type.toString())
+                .moneyAmount(moneyAmount)
+                .build();
+
+        Optional<MonetaryTransactionResponse> response = monetaryTransactionService
+                .createDepositTransaction(request);
+
+        return response.map(r -> "Successful transaction performed")
+                .orElse("Error occurred, transaction canceled");
+    }
+
+    public String withdrawFromAccount(BufferedReader reader) throws IOException {
+        System.out.println("Your accounts: ");
+        System.out.println(getMyAccounts());
+
+        System.out.print("Specify the id of an account to WITHDRAW from: ");
+        String accountId = reader.readLine();
+
+
+        List<AccountResponse> myAccounts = accountService.findAllByClientId(authenticatedUser.getId());
+        if (myAccounts.stream()
+                .filter(a -> a.getId().equals(accountId))
+                .findAny()
+                .isEmpty()) {
+            return "You can WITHDRAW only from your accounts";
+        }
+
+        System.out.print("Specify money amount to withdraw: ");
+        String moneyAmount = reader.readLine();
+
+        AccountResponse accountToDeposit = accountService.findById(accountId).get();
+        System.out.printf("Are you sure you want to WITHDRAW %s %s %nfrom %s account with balance %s?%nyes/y or no/n: ",
+                moneyAmount, accountToDeposit.getCurrency(),
+                accountId,
+                accountToDeposit.getMoneyAmount());
+        String answer = reader.readLine();
+
+        if (!answer.equals("yes") && !answer.equals("y"))
+            return "WITHDRAWAL canceled";
+
+        MonetaryTransactionType type = MonetaryTransactionType.WITHDRAWAL;
+        MonetaryTransactionRequest request = MonetaryTransactionRequest.builder()
+                .senderAccountId(accountId)
+                .receiverAccountId(null)
+                .type(type.toString())
+                .moneyAmount(moneyAmount)
+                .build();
+
+        Optional<MonetaryTransactionResponse> response = monetaryTransactionService
+                .createWithdrawalTransaction(request);
+
+        return response.map(r -> "Successful transaction performed")
+                .orElse("Error occurred, transaction canceled");
+    }
+
+    public String createNewAccount(BufferedReader reader) throws IOException {
+        System.out.printf("Creating new account for %s: %n", authenticatedUser.getEmail());
+        System.out.println(getAllBanks());
+        System.out.print("\tBank ID: ");
+        String bankIdString = reader.readLine();
+        System.out.printf("\tAccount currency(%s): ", Arrays.toString(Currency.values()));
+        String currency = reader.readLine();
+        AccountRequest request = AccountRequest.builder()
+                .currency(currency)
+                .bankId(bankIdString)
+                .clientEmail(authenticatedUser.getEmail())
+                .build();
+        String accountId = accountService.save(request);
+        return "Successfully created account with ID %s".formatted(accountId);
+    }
+
+    public String getMyAccounts() {
+        List<AccountResponse> accounts = accountService.findAllByClientId(authenticatedUser.getId());
+        return accounts.stream()
+                .map(a -> """
+                        Account ID: %s, Bank ID: %s, Money amount: %s %s, Date when opened: %s"
+                        """.formatted(
+                        a.getId(),
+                        a.getBankId(),
+                        a.getMoneyAmount(), a.getCurrency(),
+                        a.getDateWhenOpened()
+                ))
+                .collect(Collectors.joining(""));
+    }
+
+    public String getAllAccounts() {
+        List<AccountResponse> all = accountService.findAll();
+        return all.stream()
+                .map(
+                        a -> "ID: %s, EMAIL: %s, MONEY: %s%s"
+                                .formatted(a.getId(),
+                                        a.getClientEmail(),
+                                        a.getMoneyAmount(), a.getCurrency()))
+                .collect(Collectors.joining("\r\n"));
+    }
+
+    public String getAllBanks() {
+        List<BankResponse> all = bankService.findAll();
+        return all.stream()
+                .map(
+                        b -> "ID: %s, BANK NAME: %s"
+                                .formatted(b.getId(),
+                                        b.getName()))
+                .collect(Collectors.joining("\r\n"));
+    }
+}
