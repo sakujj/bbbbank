6791cd26488274ba6bb34e6fae44543d5252ca1c
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-30 23:02:42 +0300

    Update BCryptHasher

--- src/main/java/by/sakujj/hashing/BCryptHasher.java
+++ src/main/java/by/sakujj/hashing/BCryptHasher.java
@@ -1,19 +1,11 @@
 package by.sakujj.hashing;
 
+import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 
-
+@RequiredArgsConstructor
 public class BCryptHasher implements Hasher{
     private final static int LOG_ROUNDS = 10;
-
-    private static final Hasher INSTANCE = new BCryptHasher();
-
-    private BCryptHasher() {
-    }
-
-    public static Hasher getInstance() {
-        return BCryptHasher.INSTANCE;
-    }
 
     @Override
     public String hash(String password) {
